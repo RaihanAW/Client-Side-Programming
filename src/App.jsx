9732e0c707fd44080d5c0221ce38c1b794ffc214
111202214017@mhs.dinus.ac.id <111202214017@mhs.dinus.ac.id>
2025-11-03 23:59:01 +0700

    React ES6

--- src/App.jsx
+++ src/App.jsx
@@ -2,113 +2,36 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="p-6 space-y-8">
-        {/* LATIHAN 1 - NAVBAR */}
-        LATIHAN 1
-        <nav className="p-4 bg-gray-100 flex justify-between">
-          {/* Kiri - Logo */}
-          <div className="font-bold text-lg mb-2">MyLogo</div>
+    <main className="min-h-screen bg-special-mainBg flex justify-center items-center">
+      {/* container start */}
+      <div className="w-full max-w-sm">
+        {/* logo start */}
+        <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
+        </div>
+        {/* logo end */}
 
-          {/* Kanan - Menu */}
-          <ul className="md:flex flex-row p-2 gap-4">
-            <li>
-              <a href="#" className="hover:underline">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <hr />
-        {/* LATIHAN 2 - GRID GALERI */}
-        LATIHAN 2
-        <div className="p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
-          <img
-            src="https://www.placehold.co/200x200"
-            alt="Gambar Galeri"
-            className="h-48"
-          />
-          <img
-            src="https://www.placehold.co/200x200"
-            alt="Gambar Galeri"
-            className="h-48"
-          />
-          <img
-            src="https://www.placehold.co/200x200"
-            alt="Gambar Galeri"
-            className="h-48"
-          />
-          <img
-            src="https://www.placehold.co/200x200"
-            alt="Gambar Galeri"
-            className="h-48"
-          />
-          <img
-            src="https://www.placehold.co/200x200"
-            alt="Gambar Galeri"
-            className="h-48"
-          />
-          <img
-            src="https://www.placehold.co/200x200"
-            alt="Gambar Galeri"
-            className="h-48"
-          />
+        <div className="mt-16">
+          {/* form start */}
+          {/* form end */}
         </div>
-        <hr />
-        {/* LATIHAN 3 - PRICING TABLE */}
-        LATIHAN 3
-        <div className="p-4 text-center grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="bg-gray-200 p-6 rounded">
-            Basic
-            <br />
-            Rp 50.000
-          </div>
-          <div className="bg-blue-200 p-6 rounded scale-105">
-            Pro
-            <br />
-            Rp 100.000
-          </div>
-          <div className="bg-gray-200 p-6 rounded">
-            Premium
-            <br />
-            Rp 200.000
-          </div>
+
+        {/* teks start */}
+        <div className="my-9 px-7 flex flex-col justify-center items-center text-xs text-gray-03">
         </div>
-        <hr />
-        {/* LATIHAN 4 - DASHBOARD LAYOUT */}
-        LATIHAN 4
-        <div className="min-h-screen grid md:grid-cols-12 gap-1">
-          <header className="bg-gray-300 p-4 md:col-span-12">Header</header>
-          <aside className="bg-gray-200 p-4 md:col-span-3">Sidebar</aside>
-          <main className="bg-white p-4 border md:col-span-9">Content</main>
-          <footer className="bg-gray-300 p-4 md:col-span-12">Footer</footer>
+        {/* teks end */}
+
+        {/* sign in with google start */}
+        <div className="mb-8">
         </div>
-        <hr />
-        {/* LATIHAN 5 - CARD PRODUCT */}
-        LATIHAN 5
-        <div className="p-4 border flex flex-col items-center md:flex-row md:items-start md:space-x-4">
-          <img
-            src="https://www.placehold.co/80x80"
-            alt="Gambar Produk"
-            className="h-20 w-20 mb-2 md:mb-0"
-          />
-          <div className="text-center mt-4 md:text-left">
-            <h3 className="font-bold">Nama Produk</h3>
-            <p className="text-sm text-gray-600">Deskripsi produk singkat...</p>
-          </div>
+        {/* sign in with google end */}
+
+        {/* link start */}
+        <div className="flex justify-center">
         </div>
+        {/* link end */}
       </div>
-    </>
+      {/* container end */}
+    </main>
   );
 }
 
