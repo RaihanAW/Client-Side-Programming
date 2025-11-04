a9d9cb2a5b18f1a64f91dd978dde43417f19b6b4
111202214017@mhs.dinus.ac.id <111202214017@mhs.dinus.ac.id>
2025-11-04 11:15:54 +0700

    UTS Fix

--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -6,7 +6,7 @@ function PostCard(props) {
   return (
     <div className="bg-white p-6 rounded-lg shadow h-auto hover:bg-red-50 hover:border-2 hover:scale-105">
       <div className='flex flex-col items-center text-center'>
-        <h2 className="text-xl flex- font-semibold text-gray-800 mb-2">{title}</h2>
+        <h2 className="text-xl font-semibold text-gray-800 mb-2">{title}</h2>
         <p className="text-gray-600">
             <span className="font-medium">{body}</span> 
         </p>
