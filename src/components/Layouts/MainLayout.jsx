7f8b40cec8529c040e21ece9fef1f4013b6f9c59
111202214017@mhs.dinus.ac.id <111202214017@mhs.dinus.ac.id>
2025-12-16 04:10:03 +0700

    React Icons

--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Logo from "../Elements/Logo";
 import Input from "../Elements/Input";
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import Icon from "../Elements/Icon"
+import { NavLink } from "react-router-dom";
 
 
 function MainLayout(props) {
   const { children } = props;
+
+  const menu = [
+    { id: 1, name: "Overview", icon: <Icon.Main />, link: "/" },
+    { id: 2, name: "Balances", icon: <Icon.Balance />, link: "/balance" },
+    { id: 3, name: "Transaction", icon: <Icon.Transaction />, link: "/transaction", },
+    { id: 4, name: "Bills", icon: <Icon.Bill />, link: "/bill" },
+    { id: 5, name: "Expenses", icon: <Icon.Expense />, link: "/expense" },
+    { id: 6, name: "Goals", icon: <Icon.Goal />, link: "/goal" },
+    { id: 7, name: "Settings", icon: <Icon.Setting />, link: "/setting" },
+  ];
+
   
   return (
     <>
@@ -16,23 +30,27 @@ function MainLayout(props) {
                         <Logo variant="secondary"/>
                     </div>
                     <nav>
-                        <div className="flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md">
-                            <div className="mx-auto sm:mx-0">O</div>
-                            <div className="ms-3 hidden sm:block">Overview</div>
-                        </div>
-                        <div className="flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md">
-                            <div className="mx-auto sm:mx-0">B</div>
-                            <div className="ms-3 hidden sm:block">Balances</div>
-                        </div>
-                        <div className="flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md">
-                            <div className="mx-auto sm:mx-0">T</div>
-                            <div className="ms-3 hidden sm:block">Transactions</div>
-                        </div>                        
+                        {menu.map((item) => (
+                            <NavLink
+                            key={item.id}
+                            to={item.link}
+                            className={({ isActive }) =>
+                                `flex px-4 py-3 rounded-md hover:text-white hover:font-bold hover:scale-105 ${
+                                  isActive
+                                    ? "bg-primary text-white font-bold"
+                                    : "hover:bg-special-bg3"
+                                }`
+                              }
+                            >
+                            <div className="mx-auto sm:mx-0">{item.icon}</div>
+                            <div className="ms-3 hidden sm:block">{item.name}</div>
+                            </NavLink>
+                        ))}
                     </nav>
                 </div>
                 <div>
                     <div className="flex bg-special-bg3 text-white px-4 py-3 rounded-md">
-                        <div className="mx-auto sm:mx-0">L</div>
+                        <div className="mx-auto sm:mx-0"><Icon.Logout/></div>
                         <div className="ms-3 hidden sm:block">Logout</div>
                     </div>
                     <div className="border my-10 border-b-special-bg"></div>
@@ -55,7 +73,9 @@ function MainLayout(props) {
                         <div className="text-gray-03 hidden sm:block">May 19, 2023</div> 
                     </div>
                     <div className="flex items-center">
-                        <div className="me-6">Icon</div> 
+                        <div className="me-10">
+                            <CircleNotificationsIcon className="text-gray-01 scale-150" />
+                        </div> 
                     <Input backgroundColor="bg-white" border="border-white" />
                 </div>
                 </header>
